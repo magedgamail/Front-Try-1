24dcbde384a5e7423c9238a77b3f713c9a036f86
Maged Gamail <magedgamail2391@gmail.com>
2025-12-06 16:21:29 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter } from 'react-router'
+import { BrowserRouter } from 'react-router'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRoutter basename="/Front-Try-1">
         <App />
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>
   ,
 )
