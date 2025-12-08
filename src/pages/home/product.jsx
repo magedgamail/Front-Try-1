4a6d7bad0cd253bdabb8cc60a7d47b80f8937a7c
MagedGamail <magedgamail2391@gmail.com>
2025-12-08 21:46:15 +0200

    Updating

--- src/pages/home/product.jsx
+++ src/pages/home/product.jsx
@@ -35,7 +35,7 @@ export function Product({product, loadCart}) {
             <div className="product-image-container">
                 <img className="product-image"
                 data-testid="product-image"
-                    src={`${RAILWAY_API_BASE_URL}${product.image}`} />
+                    src={`${RAILWAY_API_BASE_URL}'/${product.image}`} />
             </div>
 
             <div className="product-name limit-text-to-2-lines">
